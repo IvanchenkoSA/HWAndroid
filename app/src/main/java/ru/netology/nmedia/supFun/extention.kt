145e71286da8806f3683f14fdd132b2bcf572588
IvanchenkoSA <sergey.1vanchenko@ya.ru>
2023-10-21 13:30:09 +0300

    set on clickListener

---
+++ app/src/main/java/ru/netology/nmedia/supFun/extention.kt
@@ -0,0 +1,20 @@
+package ru.netology.nmedia.supFun
+
+fun formatShortened(value: Int): String {
+    val appendix: String
+    val result: Int = when {
+        value >= 1000000 -> {
+            appendix = "M"
+            value / 1000000
+        }
+        value >= 1000 -> {
+            appendix = "K"
+            value / 1000
+        }
+        else -> {
+            appendix = ""
+            value
+        }
+    }
+    return result.toString() + appendix
+}
